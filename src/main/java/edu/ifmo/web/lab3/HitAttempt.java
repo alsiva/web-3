92f5e9a52dae9cbbb45c03d20acd7ef7baca35cb
Alex Ivanov <alexivanovrus@gmail.com>
2021-12-08 23:39:07 +0300

    add checkboxes

--- src/main/java/edu/ifmo/web/lab3/HitAttempt.java
+++ src/main/java/edu/ifmo/web/lab3/HitAttempt.java
@@ -3,35 +3,67 @@ package edu.ifmo.web.lab3;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+import static java.util.stream.Collectors.toList;
 
 @Named
 @RequestScoped
 public class HitAttempt implements Serializable {
-    private float x = 0;
-    private float y = 0;
-    private float r = 1;
+    private double x = 0;
+    private double y = 0;
 
-    public float getX() {
+    private final List<RadiusCheckBox> checkBoxes = DoubleStream.of(1.0, 1.5, 2.0, 2.5, 3.0)
+        .mapToObj(RadiusCheckBox::new)
+        .collect(toList());
+
+    public List<RadiusCheckBox> getCheckBoxes() {
+        return checkBoxes;
+    }
+
+    public double getX() {
         return x;
     }
 
-    public void setX(float x) {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public float getY() {
+    public double getY() {
         return y;
     }
 
-    public void setY(float y) {
+    public void setY(double y) {
         this.y = y;
     }
 
-    public float getR() {
-        return r;
+    public List<Double> getSelectedRadiusList() {
+        return checkBoxes.stream()
+            .filter(RadiusCheckBox::isSelected)
+            .map(RadiusCheckBox::getValue)
+            .collect(Collectors.toList());
     }
 
-    public void setR(float r) {
-        this.r = r;
+    public static class RadiusCheckBox implements Serializable {
+        private boolean isSelected = false;
+        private final double value;
+
+        public RadiusCheckBox(double value) {
+            this.value = value;
+        }
+
+        public boolean isSelected() {
+            return isSelected;
+        }
+
+        public void setSelected(boolean selected) {
+            isSelected = selected;
+        }
+
+        public double getValue() {
+            return value;
+        }
     }
 }
--- src/main/java/edu/ifmo/web/lab3/HitResult.java
+++ src/main/java/edu/ifmo/web/lab3/HitResult.java
@@ -2,8 +2,35 @@ package edu.ifmo.web.lab3;
 
 import java.io.Serializable;
 
-public class HitResult extends HitAttempt implements Serializable {
+public class HitResult implements Serializable {
+    private float x = 0;
+    private float y = 0;
+    private float r = 1;
     private boolean doesHit;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getR() {
+        return r;
+    }
+
+    public void setR(float r) {
+        this.r = r;
+    }
 
     public boolean isDoesHit() {
         return doesHit;
